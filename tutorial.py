7157efe11785689cbb12df3c030cfb54d72e38e0
Eric Song <ericksong91@gmail.com>
2024-02-08 14:54:35 -0500

    added classes

--- tutorial.py
+++ tutorial.py
@@ -1,32 +1,35 @@
 import pygame
 from pong import Game
 
-width, height = 700, 500
-window = pygame.display.set_mode((width, height))
+class PongGame:
+    def __init__(self, window, width, height):
+        self.game = Game(window, width, height)
+        self.left_paddle = self.game.left_paddle
+        self.right_paddle = self.game.right_paddle
+        self.ball = self.game.ball
 
-game = Game(window, width, height)
+    def test_ai(self):
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            clock.tick(60) # max frames to render per second
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    break
 
-run = True
-clock = pygame.time.Clock()
-while run:
-    clock.tick(60) # max frames to render per second
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-            break
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        game.move_paddle(left=True, up=True)
-    if keys[pygame.K_s]:
-        game.move_paddle(left=True, up=False)
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                game.move_paddle(left=True, up=True)
+            if keys[pygame.K_s]:
+                game.move_paddle(left=True, up=False)
 
     
-    game_info = game.loop()
-    print(game_info.left_score, game_info.right_score)
-    game.draw(False, True)
-    pygame.display.update()
+            game_info = game.loop()
+            print(game_info.left_score, game_info.right_score)
+            game.draw(False, True)
+            pygame.display.update()
 
-pygame.quit
+        pygame.quit
 
 # runs the game while run is True
